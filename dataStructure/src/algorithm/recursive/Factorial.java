3208486e3306fe2da7e444f6269649ae443726bc
hiheehee <gkek41@naver.com>
2021-08-15 15:34:51 +0900

    Factorial

--- dataStructure/src/algorithm/recursive/Factorial.java
+++ dataStructure/src/algorithm/recursive/Factorial.java
@@ -3,18 +3,29 @@ package algorithm.recursive;
 public class Factorial {
 
 	// 시간 / 공간 복잡도 O(n)
-	public int factorialFunc(int n) {
+	// 방법 1
+	public int factorialFunc1(int n) {
 		if(n > 1) {
-			return n * this.factorialFunc(n-1);
+			return n * this.factorialFunc1(n-1);
 		}else {
 			return 1;
 		}
 	}
 	
+	// 시간 / 공간 복잡도 O(n)
+	// 방법 2
+	public int factorialFunc2(int n) {
+		if(n <= 1) {
+			return 1;
+		}else {
+			return n * this.factorialFunc2(n-1);
+		}
+	}
+	
 	public static void main(String[] args) {
 		Factorial f = new Factorial();
-		System.out.println(f.factorialFunc(5));
-
+		System.out.println(f.factorialFunc1(5));
+		System.out.println(f.factorialFunc2(5));
 	}
 
 }
